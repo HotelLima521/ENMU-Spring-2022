8a40fae4057c100b6f1e465a1b0c7441d2455ff7
Derek White <white.derek26@pm.me>
2022-02-07 14:05:51 -0700

    Add classwork

---
+++ EET/EET 340/lengthOfSide.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	int a, area;
+
+	std::cout << "Enter the value of the side:\n";
+	std::cin >> a;
+
+	area = a*a;
+
+	std::cout << "The area is: " << area << std::endl;
+
+	return 0;
+}
---
+++ EET/EET 340/mathematicalExpressions.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+
+int main(){
+	int a = 10, b = 5;
+	double y;
+
+	y = sqrt(pow(a, 2) + pow(b, 2));
+
+	std::cout << "The Value of y is: " << y << std::endl;
+
+	return 0;
+}
